6ba3b9734e8aabb617fec5ca20ce8037b7368786
ChickoonLord <67387813+DavidColetta@users.noreply.github.com>
2024-08-20 18:58:37 -0400

    Initial Commit

---
+++ LifeScripter/Program.cs
@@ -0,0 +1,14 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "Life Scripter";
+
+Builder gameStartup = new Builder()
+    .SetScreenSize(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+    .SetStartingScreen<LifeScripter.Scenes.RootScreen>()
+    .IsStartingScreenFocused(true)
+    .ConfigureFonts(true)
+    ;
+
+Game.Create(gameStartup);
+Game.Instance.Run();
+Game.Instance.Dispose();
